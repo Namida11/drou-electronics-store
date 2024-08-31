82122476c5f2517e3314ec35f28dd6567d7ac9c9
Namida11 <109894273+Namida11@users.noreply.github.com>
2024-08-31 16:16:43 +0400

    create file structure

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import "./src/config/db-connect.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log("server is running PORT:", PORT);
+});
