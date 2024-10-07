451db9ef4c39e15c21e495a6533d2f6d3d73362c
Namida11 <109894273+Namida11@users.noreply.github.com>
2024-10-07 21:24:50 +0400

    cors middleware added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import "express-async-errors";
+import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
 import "./src/config/db-connect.js";
@@ -14,6 +15,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(api, route);
 app.use(errorHandlerMiddleware);
